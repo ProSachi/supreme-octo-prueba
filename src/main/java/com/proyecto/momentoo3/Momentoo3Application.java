99c4fccf939d5b055fdb0e6533c51cab0849079d
ProSachi <syosa@cesde.net>
2025-10-30 17:23:07 -0500

    creación proyecto momento3

---
+++ src/main/java/com/proyecto/momentoo3/Momentoo3Application.java
@@ -0,0 +1,13 @@
+package com.proyecto.momentoo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Momentoo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Momentoo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/momentoo3/Momentoo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.momentoo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Momentoo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
